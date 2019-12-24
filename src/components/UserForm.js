fa2e3378fded40e6a99492f74c15d9124310beeb
Annie Huang <annie.hn@gmail.com>
2019-12-24 13:10:21 +1100

    Create UserForm.js and create the state object and nextStep and prevStep method.

---
+++ src/components/UserForm.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    };
+
+    // Proceed to next step
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1,
+        });
+    };
+
+    // Proceed to prev step
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1,
+        });
+    };
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UserForm;
